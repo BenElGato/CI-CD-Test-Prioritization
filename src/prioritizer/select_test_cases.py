6d07549883c955836872675bb8eaa89c6e52d4cb
Alice Sun <alicesun@Alices-MacBook-Pro.local>
2025-05-31 13:18:04 +0900

    add select test file

--- src/prioritizer/select_test_cases.py
+++ src/prioritizer/select_test_cases.py
@@ -49,6 +49,9 @@ def main():
 
     selected_tests = select_test_cases(args.budget, args.changes, args.algorithm)
     print("Selected Test Cases:", selected_tests)
+    with open("selected_tests.txt", "w") as f:
+        for idx in selected_tests:
+            f.write(str(idx) + "\n")
 
 if __name__ == "__main__":
     main()
